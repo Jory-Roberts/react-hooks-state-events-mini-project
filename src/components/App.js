fa9dbfbc35b8d4e9742198a3fbf47e98f09be45d
Jory Roberts <robertsjory13@gmail.com>
2023-05-31 07:54:00 -0500

    adds in console logs

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,12 @@ import NewTaskForm from './NewTaskForm';
 import TaskList from './TaskList';
 
 import { CATEGORIES, TASKS } from '../data';
-// console.log("Here's the data you're working with");
-// console.log({ CATEGORIES, TASKS });
+console.log("Here's the data you're working with");
+console.log({ CATEGORIES, TASKS });
 
 function App() {
 	const [tasks, setTasks] = useState(TASKS);
-	// const [category, setCategory] = useState('CATEGORIES');
+	// const [category, setCategory] = useState(CATEGORIES);
 
 	function handleDelete(taskToDelete) {
 		setTasks((previousTasks) => previousTasks.filter((task) => task.text !== taskToDelete.text));
